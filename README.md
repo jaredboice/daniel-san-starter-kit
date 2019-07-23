702c6deab4f6d04601f25b74a51b0cea326ee35f
Jared Boice2 <jaredboice2@Jareds-iMac.local>
2019-07-22 20:35:06 -0400

    updated readme

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ budget-projection rule template for cashflow event triggers, using the daniel-sa
 ## Donations - Bitcoin: 19XgiRojJnv9VDhyW9HmF6oKQeVc7k9McU 
 (use this address until 2022)
 
+## Environment Variables
+notice in index.js that an environment variable is being used to switch between different accounts
+change this according to your budget testing needs
+
 **execution script**
 ```javascript
 npm run start
--- src/waxOnTest.js
+++ src/waxOnTest.js
@@ -1,53 +1,17 @@
-const production = process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'dev';
 const {
     STANDARD_EVENT,
-    STANDARD_EVENT_ROUTINE,
-    STANDARD_EVENT_REMINDER,
-    NTH_WEEKDAYS_OF_MONTH,
-    NTH_WEEKDAYS_OF_MONTH_ROUTINE,
-    NTH_WEEKDAYS_OF_MONTH_REMINDER,
-    WEEKDAY_ON_DATE,
-    WEEKDAY_ON_DATE_ROUTINE,
-    WEEKDAY_ON_DATE_REMINDER,
-    MOVE_THIS_PROCESS_DATE_BEFORE_THESE_WEEKDAYS,
     MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
-    MOVE_THIS_PROCESS_DATE_AFTER_THESE_WEEKDAYS,
-    MOVE_THIS_PROCESS_DATE_AFTER_THESE_DATES,
     ADJUST_AMOUNT_ON_THESE_DATES,
     ANNUALLY,
     MONTHLY,
     WEEKLY,
     DAILY,
-    ONCE,
     SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
     FRIDAY,
     SATURDAY,
     STANDARD_TERMINAL_OUTPUT,
-    SHY,
-    VERBOSE,
-    CONCISE,
-    DISPLAY_EVENTS_BY_GROUPS,
-    DISPLAY_EVENTS_BY_NAMES,
-    DISPLAY_EVENTS_BY_TYPES,
-    DISPLAY_EVENTS,
-    DISPLAY_CRITICAL_SNAPSHOTS,
-    DISPLAY_DISCARDED_EVENTS,
-    DISPLAY_IMPORTANT_EVENTS,
-    DISPLAY_TIME_EVENTS,
-    DISPLAY_ROUTINE_EVENTS,
-    DISPLAY_REMINDER_EVENTS,
-    DISPLAY_RULES_TO_RETIRE,
-    DISPLAY_SUM_OF_ALL_NEGATIVE_EVENT_AMOUNTS,
-    DISPLAY_SUM_OF_ALL_POSITIVE_EVENT_AMOUNTS,
-    DISPLAY_END_BALANCE_SNAPSHOTS_GREATER_THAN_MAX_AMOUNT,
-    DISPLAY_END_BALANCE_SNAPSHOTS_LESS_THAN_MIN_AMOUNT,
-    DISPLAY_GREATEST_END_BALANCE_SNAPSHOTS,
-    DISPLAY_LEAST_END_BALANCE_SNAPSHOTS
-} = production ? require('daniel-san/constants') : require('./danielSanDevelopment/constants');
+    CONCISE
+} = require('daniel-san/constants');
 
 const WEEKENDS = [SATURDAY, SUNDAY];
 /* BANK_HOLIDAYS
@@ -79,39 +43,51 @@ const BANK_HOLIDAYS = [
     '2019-12-24',
     '2019-12-25',
     '2019-12-31'
-];
+]; // bank holidays will need to be added for each projected year, in this case they only extend to the end of 2019
+
+const CORPORATE_HOLIDAYS = [
+    '2019-01-01',
+    '2019-07-04',
+    '2019-11-28',
+    '2019-11-29',
+    '2019-12-24',
+    '2019-12-25',
+    '2020-01-01',
+    '2020-07-04',
+    '2020-11-26',
+    '2020-11-27',
+    '2020-12-24',
+    '2020-12-25',
+    '2021-01-01'
+]; // corporate holidays will need to be added for each projected year
 
 const danielSan = {
-    beginBalance: 2445.0,
+    beginBalance: 2500.0, // see the readme for multi-currency options
     endBalance: null,
-    dateStart: '2019-09-14',
-    dateEnd: '2020-02-02',
-    rules: [],
+    dateStart: '2019-07-20',
+    dateEnd: '2021-09-11',
+    rules: [], // the rules defined below will be assigned to this array, prior to export, at the bottom of the file
     events: [] // future balance projections stored here
 };
 
 const terminalOptions = {
     type: STANDARD_TERMINAL_OUTPUT,
     mode: CONCISE,
-    selectionAmount: 10,
-    maxAmount: 1000,
-    minAmount: 500,
-    criticalThreshold: 200.00
-};
+    criticalThreshold: 150.0
+}; // see the readme for more terminal options
 
 const monthlyEssentials = [
     {
         type: STANDARD_EVENT,
         frequency: MONTHLY,
         name: 'rent',
-        amount: -1260.0,
-        dateStart: '2018-12-02',
-        dateEnd: null,
-        processDate: '01',
-        sortPriority: 10,
-        modulus: 1,
-        cycle: 1,
+        amount: -1200.0,
+        dateStart: '2019-07-20',
+        dateEnd: null, // an endDate of null signifies an ongoing accounting event
+        processDate: '01', // the 1st of each frequency, in this case it will be the 1st of every month
+        sortPriority: 10, // the lower the number the higher the sort priority (for identical processDates)
         specialAdjustments: [
+            // this special adjustment will trigger the event prior to these dates/days, which is something banks typically do for automated withdrawals
             {
                 type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
                 dates: [...BANK_HOLIDAYS],
@@ -122,14 +98,12 @@ const monthlyEssentials = [
     {
         type: STANDARD_EVENT,
         frequency: MONTHLY,
-        name: 'health insurance and dental',
-        amount: -400.0,
-        dateStart: '2018-12-02',
+        name: 'student loan',
+        amount: -150.0,
+        dateStart: '2019-07-20',
         dateEnd: null,
         processDate: '01',
         sortPriority: 20,
-        modulus: 1,
-        cycle: 1,
         specialAdjustments: [
             {
                 type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
@@ -141,33 +115,124 @@ const monthlyEssentials = [
     {
         type: STANDARD_EVENT,
         frequency: MONTHLY,
-        name: 'xfinity',
-        amount: -85.0,
-        dateStart: '2019-05-02',
-        dateEnd: null,
-        processDate: '02',
-        sortPriority: 20,
-        modulus: 1,
-        cycle: 1,
-        specialAdjustments: [
-            {
-                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
-                dates: [...BANK_HOLIDAYS],
-                weekdays: [...WEEKENDS]
-            }
-        ]
-    },
-    {
-        type: STANDARD_EVENT,
-        frequency: MONTHLY,
-        name: 'visa',
-        amount: -50.0,
-        dateStart: '2018-12-02',
+        name: 'credit card',
+        amount: -100.0,
+        dateStart: '2019-07-20',
         dateEnd: null,
         processDate: '15',
         sortPriority: 20,
         modulus: 1,
         cycle: 1,
+        specialAdjustments: [
+            {
+                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
+                dates: [...BANK_HOLIDAYS],
+                weekdays: [...WEEKENDS]
+            }
+        ]
+    },
+    {
+        type: STANDARD_EVENT,
+        frequency: MONTHLY,
+        name: 'car payment',
+        amount: -250.0,
+        dateStart: '2019-07-20',
+        dateEnd: null,
+        processDate: '02',
+        sortPriority: 20,
+        specialAdjustments: [
+            {
+                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
+                dates: [...BANK_HOLIDAYS],
+                weekdays: [...WEEKENDS]
+            }
+        ]
+    },
+    {
+        type: STANDARD_EVENT,
+        frequency: MONTHLY,
+        name: 'internet',
+        amount: -85.0,
+        dateStart: '2019-07-20',
+        dateEnd: null,
+        processDate: '02',
+        sortPriority: 20,
+        specialAdjustments: [
+            {
+                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
+                dates: [...BANK_HOLIDAYS],
+                weekdays: [...WEEKENDS]
+            }
+        ]
+    },
+    {
+        type: STANDARD_EVENT,
+        frequency: MONTHLY,
+        name: 'health insurance',
+        amount: -180.0,
+        dateStart: '2019-07-20',
+        dateEnd: null,
+        processDate: '20',
+        sortPriority: 30
+    },
+    {
+        type: STANDARD_EVENT,
+        frequency: MONTHLY,
+        name: 'car insurance',
+        amount: -100.0,
+        dateStart: '2019-07-20',
+        dateEnd: null,
+        processDate: '13',
+        sortPriority: 30
+    },
+    {
+        type: STANDARD_EVENT,
+        frequency: MONTHLY,
+        name: 'phone bill',
+        amount: -85.0,
+        dateStart: '2019-07-20',
+        dateEnd: null,
+        processDate: '28',
+        sortPriority: 20,
+        specialAdjustments: [
+            {
+                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
+                dates: [...BANK_HOLIDAYS],
+                weekdays: [...WEEKENDS]
+            }
+        ]
+    },
+    {
+        type: STANDARD_EVENT,
+        frequency: MONTHLY,
+        name: 'electric',
+        amount: -125.0,
+        dateStart: '2019-07-20',
+        dateEnd: null,
+        processDate: '17',
+        sortPriority: 20,
+        specialAdjustments: [
+            {
+                type: ADJUST_AMOUNT_ON_THESE_DATES, // we just got the bill and expect to be paying $49 less on the process date
+                dates: ['2019-07-17'],
+                amounts: [49.0] // dates and amounts are parallel arrays
+            },
+            {
+                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES, // the moving of process dates should generally come last in the array of adjustments
+                dates: [...BANK_HOLIDAYS],
+                weekdays: [...WEEKENDS]
+            }
+        ]
+    },
+    {
+        type: STANDARD_EVENT,
+        frequency: MONTHLY,
+        name: 'water',
+        amount: -20.0,
+        dateStart: '2019-07-20',
+        dateEnd: null,
+        processDate: '01',
+        sortPriority: 20,
         specialAdjustments: [
             {
                 type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
@@ -180,99 +245,25 @@ const monthlyEssentials = [
         type: STANDARD_EVENT,
         frequency: WEEKLY,
         name: 'gas',
-        amount: -75.0,
-        syncDate: '2019-06-16',
-        dateStart: '2018-12-02',
+        amount: -30.0,
+        syncDate: '2019-07-20', // specify a date to sync this trigger with a particular SUNDAY in the past or future
+        dateStart: '2019-07-20',
         dateEnd: null,
         processDate: SUNDAY,
-        modulus: 3,
-        cycle: 1
+        modulus: 3, // fill up the tank every 3rd SUNDAY (this user must live close to work)
+        cycle: 1 // the operation gets triggered when cycle reaches the modulus TODO: ?
     },
     {
         type: STANDARD_EVENT,
         frequency: MONTHLY,
         name: 'oil change',
-        amount: -75.0,
-        syncDate: '2019-08-15',
-        dateStart: '2019-08-15',
+        amount: -35.0,
+        syncDate: '2019-07-20',
+        dateStart: '2019-07-20',
         dateEnd: null,
         processDate: '15',
-        modulus: 5,
+        modulus: 3, // every third MONTHLY trigger
         cycle: 1
-    },
-    {
-        type: STANDARD_EVENT,
-        frequency: MONTHLY,
-        name: 'electric',
-        amount: -125.0,
-        dateStart: '2018-12-02',
-        dateEnd: null,
-        processDate: '19',
-        sortPriority: 20,
-        modulus: 1,
-        cycle: 1,
-        specialAdjustments: [
-            {
-                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
-                dates: [...BANK_HOLIDAYS],
-                weekdays: [...WEEKENDS]
-            }
-        ]
-    },
-    {
-        type: STANDARD_EVENT,
-        frequency: MONTHLY,
-        name: 'water',
-        amount: -20.0,
-        dateStart: '2018-12-02',
-        dateEnd: null,
-        processDate: '01',
-        sortPriority: 20,
-        modulus: 1,
-        cycle: 1,
-        specialAdjustments: [
-            {
-                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
-                dates: [...BANK_HOLIDAYS],
-                weekdays: [...WEEKENDS]
-            }
-        ]
-    },
-    {
-        type: STANDARD_EVENT,
-        frequency: MONTHLY,
-        name: 'progressive',
-        amount: -150.0,
-        dateStart: '2019-08-08',
-        dateEnd: null,
-        processDate: '13',
-        sortPriority: 30,
-        modulus: 1,
-        cycle: 1
-    },
-    {
-        type: STANDARD_EVENT,
-        frequency: MONTHLY,
-        name: 'at&t',
-        amount: -85.0,
-        dateStart: '2018-12-02',
-        dateEnd: null,
-        processDate: '28',
-        sortPriority: 20,
-        modulus: 1,
-        cycle: 1,
-        specialAdjustments: [
-            {
-                type: ADJUST_AMOUNT_ON_THESE_DATES,
-                dates: ['2019-06-28'],
-                amounts: [75.0] // dates and amounts are parallel arrays
-            },
-            {
-                type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
-                dates: [...BANK_HOLIDAYS],
-                weekdays: [...WEEKENDS]
-            }
-        ]
     }
 ];
 
@@ -280,20 +271,9 @@ const monthlyEntertainment = [
     {
         type: STANDARD_EVENT,
         frequency: MONTHLY,
-        name: 'cartridges',
-        amount: -220.0,
-        dateStart: '2019-10-15',
-        dateEnd: null,
-        processDate: '15',
-        modulus: 3,
-        cycle: 3
-    },
-    {
-        type: STANDARD_EVENT,
-        frequency: MONTHLY,
-        name: 'netflix',
-        amount: -12.5,
-        dateStart: '2018-12-02',
+        name: 'streaming service',
+        amount: -15.0,
+        dateStart: '2019-07-20',
         dateEnd: null,
         processDate: '24',
         sortPriority: 80,
@@ -309,15 +289,13 @@ const monthlyEntertainment = [
     },
     {
         type: STANDARD_EVENT,
-        frequency: ANNUALLY,
-        name: 'amazon prime',
-        amount: -125.0,
-        dateStart: '2018-06-07',
+        frequency: MONTHLY,
+        name: 'gaming subscription',
+        amount: -10.0,
+        dateStart: '2019-07-20',
         dateEnd: null,
-        processDate: '06-07',
+        processDate: '07',
         sortPriority: 80,
-        modulus: 1,
-        cycle: 1,
         specialAdjustments: [
             {
                 type: MOVE_THIS_PROCESS_DATE_BEFORE_THESE_DATES,
@@ -334,12 +312,12 @@ const weeklyEssentials = [
         frequency: WEEKLY,
         name: 'dry cleaning',
         amount: -16.0,
-        syncDate: '2018-17-12',
-        dateStart: null,
+        syncDate: null,
+        dateStart: null, // if there is no rule-level start date, daniel-san will number crunch to determine the next process date
         dateEnd: null,
         processDate: FRIDAY,
         sortPriority: 80,
-        modulus: 2,
+        modulus: 2, // every 2nd FRIDAY
         cycle: 1
     },
     {
@@ -347,20 +325,13 @@ const weeklyEssentials = [
         frequency: WEEKLY,
         name: 'groceries',
         amount: -125.0,
-        syncDate: '2018-11-27',
+        syncDate: null,
         dateStart: null,
         dateEnd: null,
         processDate: SUNDAY,
         sortPriority: 80,
-        modulus: 1,
-        cycle: 1,
-        specialAdjustments: [
-            {
-                type: ADJUST_AMOUNT_ON_THESE_DATES,
-                dates: ['2019-06-09'],
-                amounts: [125.0] // dates and amounts are parallel arrays
-            }
-        ]
+        modulus: 1, // every SUNDAY
+        cycle: 1
     }
 ];
 
@@ -369,21 +340,21 @@ const weeklyEntertainment = [
         type: STANDARD_EVENT,
         frequency: WEEKLY,
         name: 'friday entertainment',
-        amount: -40.0,
-        syncDate: '2018-11-27',
+        amount: -50.0,
+        syncDate: null,
         dateStart: null,
         dateEnd: null,
         processDate: FRIDAY,
-        sortPriority: 200,
-        modulus: 1,
+        sortPriority: 200, // low priority gets sorted last for an identical processDate
+        modulus: 1, // this allows us to opt out of friday night entertainment when we see putting us over the edge
         cycle: 1
     },
     {
         type: STANDARD_EVENT,
         frequency: WEEKLY,
         name: 'saturday entertainment',
-        amount: -40.0,
-        syncDate: '2018-11-27',
+        amount: -50.0,
+        syncDate: null,
         dateStart: null,
         dateEnd: null,
         processDate: SATURDAY,
@@ -396,38 +367,12 @@ const weeklyEntertainment = [
         frequency: WEEKLY,
         name: 'sunday entertainment',
         amount: -25.0,
-        syncDate: '2018-11-27',
+        syncDate: null,
         dateStart: null,
         dateEnd: null,
         processDate: SUNDAY,
         sortPriority: 200,
         modulus: 1,
-        cycle: 1
-    },
-    {
-        type: STANDARD_EVENT,
-        frequency: WEEKLY,
-        name: 'lost vape orion: pods',
-        amount: -15.0,
-        syncDate: '2019-06-30',
-        dateStart: '2019-06-30',
-        dateEnd: null,
-        processDate: SUNDAY,
-        sortPriority: 200,
-        modulus: 1,
-        cycle: 1
-    },
-    {
-        type: STANDARD_EVENT,
-        frequency: WEEKLY,
-        name: 'lost vape orion: cbd oil',
-        amount: -55.0,
-        syncDate: '2019-07-07',
-        dateStart: '2019-07-07',
-        dateEnd: null,
-        processDate: SUNDAY,
-        sortPriority: 200,
-        modulus: 3,
         cycle: 1
     }
 ];
@@ -436,10 +381,10 @@ const biweeklyChecks = [
     {
         type: STANDARD_EVENT,
         frequency: WEEKLY,
-        name: 'florida blue income',
-        amount: 1622.00,
-        syncDate: '2018-12-02',
-        dateStart: '2018-12-02',
+        name: 'paycheck',
+        amount: 1671.25,
+        syncDate: '2019-07-20',
+        dateStart: '2019-07-20',
         dateEnd: null,
         processDate: FRIDAY,
         sortPriority: 50,
@@ -460,21 +405,41 @@ const annualBills = [
         type: STANDARD_EVENT,
         frequency: ANNUALLY,
         name: 'New Years Blast',
-        amount: -30.0,
-        dateStart: '2018-12-01',
+        amount: -50.0,
+        dateStart: '2019-07-20',
         dateEnd: null,
         processDate: '12-31',
         sortPriority: 200
     }
 ];
 
+const weekDayBills = [
+    {
+        type: STANDARD_EVENT,
+        frequency: DAILY,
+        name: 'work cafeteria allowance',
+        amount: -7.5,
+        dateStart: '2019-07-20',
+        dateEnd: null,
+        sortPriority: 80,
+        exclusions: {
+            // exclude these dates/days
+            weekdays: [SATURDAY, SUNDAY],
+            dates: [...CORPORATE_HOLIDAYS]
+        },
+        modulus: 1,
+        cycle: 1
+    }
+];
+
 const rules = [
+    ...annualBills,
     ...monthlyEssentials,
     ...monthlyEntertainment,
+    ...biweeklyChecks,
     ...weeklyEssentials,
     ...weeklyEntertainment,
-    ...biweeklyChecks,
-    ...annualBills
+    ...weekDayBills
 ];
 
 danielSan.rules = rules;
